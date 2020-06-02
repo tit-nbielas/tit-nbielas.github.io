c54beb52574678df3e0855b5990d96a896e71a14
tit-nbielas <64528780+tit-nbielas@users.noreply.github.com>
2020-06-02 14:01:29 +0200

    Add files via upload

--- skrypt.js
+++ skrypt.js
@@ -6,3 +6,68 @@ function zmien2()
 {
 	document.getElementById('foto').src="foto2.png";
 }
+function zmien3 ()
+{	
+		document.getElementById('pies').src="foto3.png";
+}
+function zmien4 ()
+{
+		document.getElementById('pies').src="foto4.png";
+}
+function zmien5 ()
+{
+	document.getElementById('kot').src="foto5.png";
+}
+function zmien6 ()
+{
+	document.getElementById('kot').src="foto6.png";
+}
+function zmien7 ()
+{
+	document.getElementById('zubr').src="foto7.png";
+}
+function zmien8 ()
+{
+	document.getElementById('zubr').src="foto8.png";
+}
+function zmien9 ()
+{
+	document.getElementById('zbik').src="foto9.png";
+}
+function zmien10 ()
+{
+	document.getElementById('zbik').src="foto10.png";
+}
+function zmien11 ()
+{
+	document.getElementById('hipopotam').src="foto11.png";
+}
+function zmien12 ()
+{
+	document.getElementById('hipopotam').src="foto12.png";
+}
+function zmien13 ()
+{
+	document.getElementById('lew').src="foto13.png";
+}
+function zmien14 ()
+{
+	document.getElementById('lew').src="foto14.png";
+}
+function zmien15 ()
+{
+	document.getElementById('niedzwiedz').src="foto15.png";
+}
+function zmien16 ()
+{
+	document.getElementById('niedzwiedz').src="foto16.png";
+}
+function zmien17 ()
+{
+	document.getElementById('hiena').src="foto17.png";
+}
+function zmien18 ()
+{
+	document.getElementById('hiena').src="foto18.png";
+}
+
